dc013f975782e332178c2876ef98cef5c8a43b26
Allan Ochieng <alnothigo@gmail.com>
2024-11-19 11:46:00 +0300

    Initial files upload
    New files

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Put the bio in here</p>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {/* add your <Links /> component here */}
+    </div>
+  );
+}
+
+export default About;
